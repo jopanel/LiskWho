1ed843ecf54f9a576cb3577b9ebb9092be1cb66f
Joseph F. Opanel IV <opanelj@yahoo.com>
2017-11-19 11:06:51 -0800

    updiggity

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'default2';
 $query_builder = TRUE;
 
 $db['default'] = array(
@@ -78,6 +78,28 @@ $db['default'] = array(
 	'hostname' => '127.0.0.1',
 	'username' => 'hbjus',
 	'password' => '16858',
+	'database' => 'lsk',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['default2'] = array(
+	'dsn'	=> '',
+	'hostname' => '127.0.0.1',
+	'username' => 'root',
+	'password' => '',
 	'database' => 'lsk',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
